6ef5abdd9077deb21f8137d694d8abbe295709a7
siiinbad <nimue.jumaoas@gmai.com>
2026-03-11 22:23:09 +0800

    tweaked some accessors

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -26,11 +26,7 @@ public class GamePanel extends JPanel implements Runnable {
     Thread gameThread;
     public Player player;
 
-<<<<<<< HEAD
     BufferedImage mapImage;
-=======
-  Rectangle ivanBtn, nimuelBtn, samBtn, johnBtn;
->>>>>>> 32b2be8b2ca8978cbfb7a5ce2c6bcd96933f7c8e
 
     // Game states
     public int gameState;
@@ -43,7 +39,6 @@ public class GamePanel extends JPanel implements Runnable {
     // Menu buttons
     Rectangle startBtn, selectCharBtn, libraryBtn, exitBtn;
 
-<<<<<<< HEAD
     // Character select screen
     SelectCharacter charSelect;
 
@@ -83,126 +78,17 @@ public class GamePanel extends JPanel implements Runnable {
         } catch (Exception e) {
             System.out.println("Map image failed to load, using placeholder.");
         }
-=======
-    this.addMouseListener(new MouseAdapter() {
-      @Override
-      public void mousePressed(MouseEvent e) {
-        if (gameState == titleState) {
-          if (ivanBtn.contains(e.getPoint())) selectChar("ivan");
-          else if (nimuelBtn.contains(e.getPoint())) selectChar("nimuel");
-          else if (samBtn.contains(e.getPoint())) selectChar("sam");
-          else if (johnBtn.contains(e.getPoint())) selectChar("john");
-        }
-      }
-    });
-  }
-
-  private void initButtons() {
-    int btnW = 300;
-    int btnH = 60;
-    int centerX = screenWidth / 2 - btnW / 2;
-
-    ivanBtn = new Rectangle(centerX, 300, btnW, btnH);
-    nimuelBtn = new Rectangle(centerX, 400, btnW, btnH);
-    samBtn = new Rectangle(centerX, 500, btnW, btnH);
-    johnBtn = new Rectangle(centerX, 600, btnW, btnH);
-  }
-
-  private void loadImages() {
-    try {
-      mapImage = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/res/sprites/map/gle.png")));
-    } catch (Exception e) {
-      System.out.println("Image loading failed, using color placeholders.");
-    }
-  }
-
-  private void selectChar(String name) {
-    player = new Player(this, keyH, name);
-    gameState = playState;
-  }
-
-  public void startGameThread() {
-    gameThread = new Thread(this);
-    gameThread.start();
-  }
-
-  @Override
-  public void run() {
-    double drawInterval = 1000000000 / fps;
-    double delta = 0;
-    long lastTime = System.nanoTime();
-    while (gameThread != null) {
-      long currentTime = System.nanoTime();
-      delta += (currentTime - lastTime) / drawInterval;
-      lastTime = currentTime;
-      if (delta >= 1) {
-        update();
-        repaint();
-        delta--;
-      }
-    }
-  }
-
-  public void update() {
-    if (gameState == playState) {
-      player.update();
-      // Return to menu if ESC is pressed
-      if (keyH.escPressed) {
-        gameState = titleState;
-      }
-    }
-  }
-
-  @Override
-  public void paintComponent(Graphics g) {
-    super.paintComponent(g);
-    Graphics2D g2 = (Graphics2D) g;
-
-    if (gameState == titleState) {
-      drawMenu(g2);
-    } else if (gameState == playState) {
-      if (mapImage != null) {
-        g2.drawImage(mapImage, 0, 0, getWidth(), getHeight(), null);
-      } else {
-        g2.setColor(Color.DARK_GRAY);
-        g2.fillRect(0, 0, getWidth(), getHeight());
-      }
-      player.draw(g2);
-
-      // Visual indicator for returning to menu
-      g2.setColor(Color.WHITE);
-      g2.setFont(new Font("Arial", Font.PLAIN, 20));
-      g2.drawString("ESC to Menu", 20, 30);
-    }
-    g2.dispose();
-  }
-
-  private void drawMenu(Graphics2D g2) {
-    if (menuImage != null) {
-      g2.drawImage(menuImage, 0, 0, getWidth(), getHeight(), null);
-    } else {
-      g2.setColor(new Color(50, 50, 80));
-      g2.fillRect(0, 0, getWidth(), getHeight());
->>>>>>> 32b2be8b2ca8978cbfb7a5ce2c6bcd96933f7c8e
     }
 
     private void initMenuButtons() {
         int btnW = 300, btnH = 60;
         int centerX = screenWidth / 2 - btnW / 2;
 
-<<<<<<< HEAD
         startBtn = new Rectangle(centerX, 250, btnW, btnH);
         selectCharBtn = new Rectangle(centerX, 350, btnW, btnH);
         libraryBtn = new Rectangle(centerX, 450, btnW, btnH);
         exitBtn = new Rectangle(centerX, 550, btnW, btnH);
     }
-=======
-    drawBtn(g2, "IVAN", ivanBtn);
-    drawBtn(g2, "NIMUEL", nimuelBtn);
-    drawBtn(g2, "SAM", samBtn);
-    drawBtn(g2,  "JOHN", johnBtn);
-  }
->>>>>>> 32b2be8b2ca8978cbfb7a5ce2c6bcd96933f7c8e
 
     private void handleMenuClick(Point p) {
         if (startBtn.contains(p)) {
